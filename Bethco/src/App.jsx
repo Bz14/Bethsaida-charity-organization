1721ae25bd82186d596eb551f39a944b4bf73d6b
Bz14 <bezueyerusalem@gmail.com>
2025-03-03 22:58:31 +0300

    Sample header and footr addeded.

--- Bethco/src/App.jsx
+++ Bethco/src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
+import Navbar from "./components/nav";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
